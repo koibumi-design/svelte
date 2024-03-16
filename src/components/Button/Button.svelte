f75ce7e565f2dccf8e7a037aa9d25ce93fd65277
haruki <meteo.haru@gmail.com>
2024-03-16 16:38:43 +0000

    init

---
+++ src/components/Button/Button.svelte
@@ -0,0 +1,36 @@
+<script lang="ts">
+    import styles from './button.module.scss';
+    import classNames from "classnames";
+
+    type Variant = 'solid' | 'light' | 'outline' | 'flat' | 'ghost' | 'glow';
+    type Color = 'primary'
+
+    export const variant: Variant = 'solid';
+    export const color: Color = 'primary';
+    export const darkMode = false;
+    export const disabled = false;
+
+    const mainClass = styles[`button-${variant}-${color}`];
+
+    let classList = '';
+
+    $: classList = classNames(
+        $$props.class,
+        mainClass,
+        {
+            [styles['button-disabled']]: disabled
+        },
+        {
+            [styles['dark-mode']]: darkMode
+        },
+
+    )
+</script>
+
+<button
+        {...$$restProps}
+        class={classList}
+        disabled={disabled}
+>
+    <slot/>
+</button>
